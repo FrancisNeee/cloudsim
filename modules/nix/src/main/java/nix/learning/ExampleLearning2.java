12abf1d84da628f2d3130dabf8ccc44d7988c8d1
FrancisNee <francisnee@hotmail.com>
2024-04-21 11:39:39 +0800

    example-learning 2

--- modules/nix/src/main/java/nix/learning/ExampleLearning2.java
+++ modules/nix/src/main/java/nix/learning/ExampleLearning2.java
@@ -1,13 +1,10 @@
-/*
- * Title:        CloudSim Toolkit
- * Description:  CloudSim (Cloud Simulation) Toolkit for Modeling and Simulation
- *               of Clouds
- * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
- *
- * Copyright (c) 2009, The University of Melbourne, Australia
- */
+package nix.learning;
 
-package org.cloudbus.cloudsim.examples;
+import org.cloudbus.cloudsim.*;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -15,30 +12,13 @@ import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.cloudbus.cloudsim.Cloudlet;
-import org.cloudbus.cloudsim.CloudletSchedulerTimeShared;
-import org.cloudbus.cloudsim.Datacenter;
-import org.cloudbus.cloudsim.DatacenterBroker;
-import org.cloudbus.cloudsim.DatacenterCharacteristics;
-import org.cloudbus.cloudsim.Host;
-import org.cloudbus.cloudsim.Log;
-import org.cloudbus.cloudsim.Pe;
-import org.cloudbus.cloudsim.Storage;
-import org.cloudbus.cloudsim.UtilizationModel;
-import org.cloudbus.cloudsim.UtilizationModelFull;
-import org.cloudbus.cloudsim.Vm;
-import org.cloudbus.cloudsim.VmAllocationPolicySimple;
-import org.cloudbus.cloudsim.VmSchedulerTimeShared;
-import org.cloudbus.cloudsim.core.CloudSim;
-import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
-
 /**
- * A simple example showing how to create a datacenter with one host and run two cloudlets on it. The cloudlets run in
- * VMs with the same MIPS requirements. The cloudlets will take the same time to complete the execution.
+ * CloudSimExample2 一个Host运行两个cloudlets
+ *
+ * @Author FrancisNee
+ * @Date 2024/04/21 11:25:20
  */
-public class CloudSimExample2 {
+public class ExampleLearning2 {
 
     /** The cloudlet list. */
     private static List<Cloudlet> cloudletList;
@@ -54,90 +34,59 @@ public class CloudSimExample2 {
         Log.printLine("Starting CloudSimExample2...");
 
         try {
-            // First step: Initialize the CloudSim package. It should be called
-            // before creating any entities.
-            int num_user = 1;   // number of cloud users
+            // 初始化
+            int num_user = 1;
             Calendar calendar = Calendar.getInstance();
-            boolean trace_flag = false;  // mean trace events
-
-            // Initialize the CloudSim library
+            boolean trace_flag = false;
             CloudSim.init(num_user, calendar, trace_flag);
-
-            // Second step: Create Datacenters
-            //Datacenters are the resource providers in CloudSim. We need at list one of them to run a CloudSim simulation
+            // 创建数据中心
             @SuppressWarnings("unused") Datacenter datacenter0 = createDatacenter("Datacenter_0");
-
-            //Third step: Create Broker
+            // 创建数据中心代理
             DatacenterBroker broker = createBroker();
             int brokerId = broker.getId();
-
-            //Fourth step: Create one virtual machine
-            vmlist = new ArrayList<Vm>();
-
-            //VM description
+            // 创建2个配置相同的VM
+            vmlist = new ArrayList<>();
             int vmid = 0;
             int mips = 250;
-            long size = 10000; //image size (MB)
-            int ram = 512; //vm memory (MB)
+            long size = 10000;
+            int ram = 512;
             long bw = 1000;
-            int pesNumber = 1; //number of cpus
-            String vmm = "Xen"; //VMM name
-
-            //create two VMs
+            int pesNumber = 1;
+            String vmm = "Xen";
             Vm vm1 = new Vm(vmid, brokerId, mips, pesNumber, ram, bw, size, vmm, new CloudletSchedulerTimeShared());
-
             vmid++;
             Vm vm2 = new Vm(vmid, brokerId, mips, pesNumber, ram, bw, size, vmm, new CloudletSchedulerTimeShared());
-
-            //add the VMs to the vmList
             vmlist.add(vm1);
             vmlist.add(vm2);
-
-            //submit vm list to the broker
+            // 提交VM
             broker.submitVmList(vmlist);
-
-            //Fifth step: Create two Cloudlets
-            cloudletList = new ArrayList<Cloudlet>();
-
-            //Cloudlet properties
+            // 创建2个相同的cloudlets
+            cloudletList = new ArrayList<>();
             int id = 0;
             pesNumber = 1;
             long length = 250000;
             long fileSize = 300;
             long outputSize = 300;
             UtilizationModel utilizationModel = new UtilizationModelFull();
-
             Cloudlet cloudlet1 = new Cloudlet(id, length, pesNumber, fileSize, outputSize, utilizationModel,
                 utilizationModel, utilizationModel);
             cloudlet1.setUserId(brokerId);
-
             id++;
             Cloudlet cloudlet2 = new Cloudlet(id, length, pesNumber, fileSize, outputSize, utilizationModel,
                 utilizationModel, utilizationModel);
             cloudlet2.setUserId(brokerId);
-
-            //add the cloudlets to the list
             cloudletList.add(cloudlet1);
             cloudletList.add(cloudlet2);
-
-            //submit cloudlet list to the broker
+            // 提交cloudlet
             broker.submitCloudletList(cloudletList);
-
-            //bind the cloudlets to the vms. This way, the broker
-            // will submit the bound cloudlets only to the specific VM
+            // 分配clodlet到指定的vm
             broker.bindCloudletToVm(cloudlet1.getCloudletId(), vm1.getId());
             broker.bindCloudletToVm(cloudlet2.getCloudletId(), vm2.getId());
-
-            // Sixth step: Starts the simulation
+            // 仿真
             CloudSim.startSimulation();
-
-            // Final step: Print results when simulation is over
             List<Cloudlet> newList = broker.getCloudletReceivedList();
-
             CloudSim.stopSimulation();
-
             printCloudletList(newList);
-
             Log.printLine("CloudSimExample2 finished!");
         } catch (Exception e) {
             e.printStackTrace();
@@ -244,4 +193,5 @@ public class CloudSimExample2 {
         }
 
     }
+
 }
